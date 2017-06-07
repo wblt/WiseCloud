95a461c1352978514a19c399d37077766c5d6b1f
mac <wb@mac.local>
2017-06-07 15:31:12 +0800

    智云健康

---
+++ MVVM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVM
+//
+//  Created by mac on 2017/6/7.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+#use_frameworks!个别需要用到它，比如reactiveCocoa
+
+target 'MVVM' do
+  pod 'AFNetworking', '~> 3.1.0'
+  pod 'SDWebImage', '~> 4.0.0'
+  pod 'SVProgressHUD', '~> 2.1.2'
+end
